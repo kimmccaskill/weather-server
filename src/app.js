8c07acd7896a59966d533f1133480163a167befb
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-29 13:05:58 -0600

    Add routes to render handlebars files

--- src/app.js
+++ src/app.js
@@ -3,7 +3,29 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Kim McCaskill'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Kim McCaskill'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Page',
+    message: 'If you need help, you might be out of luck'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
